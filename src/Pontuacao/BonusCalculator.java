3e459aba4f5e4637f9d2f00fe7b7967f1f1e5219
Wendson <wendsonsantanajps@gmail.com>
2021-12-25 23:38:57 -0300

    fixbugs

--- src/Pontuacao/BonusCalculator.java
+++ src/Pontuacao/BonusCalculator.java
@@ -3,12 +3,12 @@ package Pontuacao;
 
 public class BonusCalculator {
 
-    public int bonusDay;
+    public int bonusDay = 1;
 
     public int bonusUser(User u) {
         int multiplier = bonusDay;
         if (u.vip) {
-            multiplier = 5;
+            multiplier *= 5;
         }
         return multiplier;
     }
